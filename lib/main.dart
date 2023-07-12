0d3c622a330bde369822261f528a6b6edacb0b01
3khaled3 <ff.tark123@gmail.com>
2023-07-12 23:51:17 +0300

    save

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:foryou/features/login/presentation/login_view.dart';
 import 'package:foryou/features/register/presentation/register_view.dart';
 
+import 'features/forgetpssword/presentation/forgetpass_view.dart';
+
 void main()
 {
   runApp(MaterialApp(debugShowCheckedModeBanner: false,
-    home: loginView(),
-    // home: registerView(),
+    // home: loginView(),
+    home: forgetPssView(),
   ));
 }
