7a6939fcc0d12f1f8f26b0a87782ab15b97faad1
3khaled3 <ff.tark123@gmail.com>
2023-07-12 21:35:42 +0300

    makeregisterscreen

---
+++ lib/features/register/presentation/register_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../../../constant.dart';
+import '../../../core/widget/arrowAppBar.dart';
+import '../../../core/widget/customElevationButtom.dart';
+import '../../../core/widget/customTextFaild.dart';
+import '../../../core/widget/googlebuttom.dart';
+import '../../../core/widget/headertext.dart';
+import '../../../core/widget/textbuttom.dart';
+
+// ignore: camel_case_types
+class registerView extends StatelessWidget {
+  const registerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      appBar: arrowappbar(onPressed: () {}),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            headerText(titel: "Sign up"),
+            const Spacer(flex: 1),
+            customTextfaild(labelText: "Name"),
+            const SizedBox(
+              height: 10,
+            ),
+            customTextfaild(labelText: "Email"),
+            const SizedBox(
+              height: 10,
+            ),
+            customTextfaild(labelText: "Password"),
+            textbuttom(text: "Already have an account?", onPressed: () {}),
+            const SizedBox(
+              height: 8,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: customElevationButtom(text: "SIGN UP", onPressed: () {}),
+            ),
+            const Spacer(flex: 3),
+            const Text("Or sign up with"),
+            googlebuttom(onPressed: () {})
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:foryou/features/login/presentation/login_view.dart';
+import 'package:foryou/features/register/presentation/register_view.dart';
 
 void main()
 {
   runApp(MaterialApp(debugShowCheckedModeBanner: false,
-    home: loginView(),
+    // home: loginView(),
+    home: registerView(),
   ));
 }
