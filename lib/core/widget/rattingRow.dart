22f53d8e0bd882c8c6cdadc3d94f88e06ce8e787
3khaled3 <ff.tark123@gmail.com>
2023-07-13 12:19:11 +0300

    createratingRow

---
+++ lib/core/widget/rattingRow.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class RatingRow extends StatelessWidget {
+  final int rating;
+  final Color starColor;
+  final double iconSize;
+  final int ratingNum;
+
+  const RatingRow({
+    required this.rating,
+    required this.ratingNum,
+    this.starColor = Colors.yellow,
+    this.iconSize = 24.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Row(
+          children: List.generate(5, (index) {
+            if (index < rating) {
+              return Icon(
+                Icons.star,
+                color: starColor,
+                size: iconSize,
+              );
+            } else {
+              return Icon(
+                Icons.star_border,
+                color: starColor,
+                size: iconSize,
+              );
+            }
+          }),
+        ),
+        Text(
+          "($ratingNum)",
+          style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 10,
+              fontWeight: FontWeight.normal),
+        )
+      ],
+    );
+  }
+}
--- lib/features/home/presentation/home_view.dart
+++ lib/features/home/presentation/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foryou/core/widget/rattingRow.dart';
 import 'package:foryou/features/home/presentation/widget/homeCatogrey.dart';
 import 'package:foryou/features/home/presentation/widget/titelCatogry.dart';
 import '../../../constant.dart';
@@ -19,13 +20,53 @@ class homeView extends StatelessWidget {
             SizedBox(
               height: MediaQuery.sizeOf(context).height * .03,
             ),
-            titelcatogrey(titel: "Sale",supTitel: "Super summer sale",onPressed: (){},),
+            titelcatogrey(
+              titel: "Sale",
+              supTitel: "Super summer sale",
+              onPressed: () {},
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    height: 260,
+                    width: 150,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.asset(
+                                    "assets/styles/2.jpg",
+                                    height: 184,
+                                  ),
+                                  RatingRow(
+                                    rating: 2,
+                                    ratingNum: 22,
+                                    iconSize: 16.00,
+                                    starColor: Color(0xffFFBA49),
+                                  )
+                                ],
+                              ),
+                            ),
+                            color: Colors.white,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
-
