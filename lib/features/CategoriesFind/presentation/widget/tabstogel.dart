994ee5433006aad8f8f6e2ea8d31c5569f528416
3khaled3 <ff.tark123@gmail.com>
2023-07-28 04:31:59 +0300

    buildveiwallprodctview

--- lib/features/CategoriesFind/presentation/widget/tabstogel.dart
+++ lib/features/CategoriesFind/presentation/widget/tabstogel.dart
@@ -1,159 +1,85 @@
 import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foryou/core/utils/routes.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../core/utils/Cubits/Apicubit/api_cubit.dart';
 
 class tabstogel extends StatelessWidget {
+  final int initial;
   const tabstogel({
+    required this.initial,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
+    late TabController _tabController;
+    List categorieslist = BlocProvider.of<ApiCubit>(context).categories;
+    List<Widget> taps = [];
+    taps.add(const Tab(
+      child: Tap(tap: "All"),
+    ));
+    for (var i = 0; i < categorieslist.length; i++) {
+      taps.add(Tab(
+        child: Tap(tap: categorieslist[i]),
+      ));
+    }
+
+    _tabController = TabController(
+      length: taps.length,
+      initialIndex: initial,
+      vsync: Scaffold.of(context),
+    );
     return DefaultTabController(
-      length: 10,
+      length: taps.length,
       child: Column(
         children: <Widget>[
           SafeArea(
             child: ButtonsTabBar(
-              radius: 100,
-              onTap: (value) {
-                print(value);
-              },
-              backgroundColor: Colors.red,
-              unselectedBackgroundColor: Colors.black,
-              unselectedLabelStyle: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500),
-              labelStyle: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500),
-              tabs: const [
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-                Tab(
-                  child: SizedBox(
-                      width: 100,
-                      child: Center(
-                          child: Text(
-                        "New",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ))),
-                ),
-              ],
-            ),
+                controller: _tabController,
+                radius: 100,
+                onTap: (value) {
+                  GoRouter.of(context).pop();
+                  GoRouter.of(context).push(AppRouter.kallproductsView,
+                      extra: categorieslist[(value - 1)]);
+                },
+                backgroundColor: Colors.red,
+                unselectedBackgroundColor: Colors.black,
+                unselectedLabelStyle: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500),
+                labelStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500),
+                tabs: taps),
           ),
         ],
       ),
+    );
+  }
+}
+
+class Tap extends StatelessWidget {
+  final String tap;
+  const Tap({
+    required this.tap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Center(
+          child: Text(
+        tap,
+        style: const TextStyle(
+            color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500),
+      )),
     );
   }
 }
